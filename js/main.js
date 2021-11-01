5fb3c795f7078f24c44b4d53fc6386f866b6cbf5
JellinekGy <90341733+JellinekGy@users.noreply.github.com>
2021-11-01 11:15:14 +0100

    Changed the code but still not working :(

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 'use strict';
 
-let buttons = document.getElementsByClassName('button');
-
-function displayButtonName (x) {
-    x = buttons.innerText;
-    console.log(x);
-}
+let buttons = document.querySelectorAll(".button");
 
 let handleClick = () => {
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click',displayButtonName)
-    }
+    console.log(this.textContent);
+}
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', handleClick);
 }
